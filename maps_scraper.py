852e7fdd4293a69dadf3aa5e929fcbdd5ca758da
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2025-04-10 23:08:54 +0530

    Update maps_scraper.py

--- maps_scraper.py
+++ maps_scraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
@@ -8,7 +8,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-CHROMIUM_PATH = "/usr/bin/chromium-browser"
+# Chrome path config for Streamlit Cloud
+CHROMIUM_PATH = "/usr/bin/google-chrome-stable"
 CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
 
 def get_email_from_website(url):
@@ -27,10 +28,10 @@ def scrape_google_maps(keyword, location):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("user-agent=Mozilla/5.0")
 
     service = Service(executable_path=CHROMEDRIVER_PATH)
     driver = webdriver.Chrome(service=service, options=options)
+
     driver.get("https://www.google.com/maps")
     time.sleep(2)
 
