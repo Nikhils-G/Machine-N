afb47839ff10666d3a1dc853bc58ef94c3f97f0b
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2025-04-10 23:24:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+apt-get update
+apt-get install -y wget unzip xvfb libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2 libxshmfence1 libxss1 libx11-xcb1 libxtst6
+
 python -m playwright install --with-deps
